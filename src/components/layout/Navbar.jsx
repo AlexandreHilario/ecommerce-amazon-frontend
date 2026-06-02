a8de594902582ae6557fdf419f94bda40655861b
Beatriz Pagliani <beatrizsales.pcc@gmail.com>
2026-06-01 22:04:11 -0300

    feat: adiciona header, navbar e banner da home

---
+++ src/components/layout/Navbar.jsx
@@ -0,0 +1,26 @@
+export default function Navbar() {
+    const categories = [
+    "Eletrônicos",
+    "Games",
+    "Livros",
+    "Moda",
+    "Informática",
+  ];
+
+  return (
+    <nav className="bg-[#232f3e] text-white">
+      <div className="max-w-7xl mx-auto flex gap-6 px-4 py-2">
+        {categories.map((item) => (
+          <a
+            key={item}
+            href="#"
+            className="hover:underline"
+          >
+            {item}
+          </a>
+        ))}
+      </div>
+    </nav>
+  );
+
+}
